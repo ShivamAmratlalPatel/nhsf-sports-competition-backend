5819d64942093fc635c2d15887a1c389764358dc
Shivam Patel <shivam@tenancy360.co.uk>
2023-09-12 22:18:26 +0100

    Fixed ruff and black errors

--- backend/matches/mathes_commands/log_score.py
+++ backend/matches/mathes_commands/log_score.py
@@ -1,5 +1,18 @@
+"""Log the score of a match."""
+from uuid import UUID
 
-def log_score(match_id, home_score, away_score, db):
+from sqlalchemy.orm import Session
+
+from backend.matches.matches_models import Match
+
+
+def log_score(
+    match_id: UUID,
+    home_score: float,
+    away_score: float,
+    db: Session,
+) -> None:
+    """Log the score of a match."""
     match = db.get(Match, match_id)
     match.home_score = home_score
     match.away_score = away_score
