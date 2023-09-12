7560c40e0b2e2b1b7b70b5cee04eed91c3cf23a4
Dhruv Soni <dhruvsoni1299@gmail.com>
2023-09-12 22:04:02 +0100

    implemented function

--- backend/matches/mathes_commands/log_score.py
+++ backend/matches/mathes_commands/log_score.py
@@ -1,3 +1,7 @@
 
-def log_score(match_id, home_score, away_score):
-    raise NotImplementedError
+def log_score(match_id, home_score, away_score, db):
+    match = db.get(Match, match_id)
+    match.home_score = home_score
+    match.away_score = away_score
+    db.add(match)
+    db.commit()
